d8261ad01255cfee7c148bbcdf7c3eee5facbf2d
sinelnic <sinelnic@yahoo.com>
2020-05-14 19:57:14 -0300

    Update config.js sabor sin APIM

--- beer-catalog-demo/api-consumer/config.js
+++ beer-catalog-demo/api-consumer/config.js
@@ -6,12 +6,12 @@
 define(function(require, exports, module) {
   module.exports = {
     // The base URL of the Beer API
-    baseURL: "http://beer-catalog-api-beer-catalog-prod.52.174.149.59.nip.io/api",
+    baseURL: "http://api-beer-catalog/api",
 
     // If a user-key is required, you can pass it as HTTP Header:
-    additionalHeaders: { "user-key": "db0a8a7d3ca99b5df5aace2f61ac4e45" },
+    //additionalHeaders: { "user-key": "db0a8a7d3ca99b5df5aace2f61ac4e45" },
 
     // Or in the Query String
-    extraQueryStringSuffix: "user_key=db0a8a7d3ca99b5df5aace2f61ac4e45"
+    //extraQueryStringSuffix: "user_key=db0a8a7d3ca99b5df5aace2f61ac4e45"
   };
 });
