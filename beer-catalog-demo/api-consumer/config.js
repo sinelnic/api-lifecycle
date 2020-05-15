576e1db377d02a3763b6813f5f93a5824362468d
sinelnic <sinelnic@yahoo.com>
2020-05-14 21:05:26 -0300

    Update config.js

--- beer-catalog-demo/api-consumer/config.js
+++ beer-catalog-demo/api-consumer/config.js
@@ -6,10 +6,10 @@
 define(function(require, exports, module) {
   module.exports = {
     // The base URL of the Beer API
-    baseURL: "https://beer-catalog-mock-3scale-apicast-production.apps.cluster-telecom-4fc0.telecom-4fc0.example.opentlc.com:443"
+    baseURL: "https://beer-catalog-mock-3scale-apicast-production.apps.cluster-telecom-4fc0.telecom-4fc0.example.opentlc.com:443",
 
     // If a user-key is required, you can pass it as HTTP Header:
-    additionalHeaders: { "user-key": "43778b9e692bd8626363f7ad88821391" },
+    additionalHeaders: { "user-key": "43778b9e692bd8626363f7ad88821391" }
 
     // Or in the Query String
     //extraQueryStringSuffix: "user_key=db0a8a7d3ca99b5df5aace2f61ac4e45"
